51800fcd0ef1a73bcd1088b3720d4e43bf7033ce
willscos <costaws2303@gmail.com>
2026-05-17 19:59:19 -0300

    Update database.py

--- app/models/database.py
+++ app/models/database.py
@@ -4,7 +4,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não encontrada nas variáveis de ambiente")
+
+# Neon exige SSL
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -17,7 +25,8 @@ def get_db():
         db.close()
 
 def init_db():
+    # Importa os modelos para registrar no metadata
     from app.models.transacao import Transacao
     from app.models.categoria import Categoria
-    from app.models.usuario import Usuario  # <-- ESSENCIAL
+    from app.models.usuario import Usuario
     Base.metadata.create_all(bind=engine)
