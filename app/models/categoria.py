ae85cd3d34c15528551c5ba2941664b5a54a73fa
willscos <costaws2303@gmail.com>
2026-05-17 21:03:44 -0300

    Update categoria.py

--- app/models/categoria.py
+++ app/models/categoria.py
@@ -1,8 +1,16 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from app.models.database import Base
 
 class Categoria(Base):
     __tablename__ = "categorias"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, unique=True, nullable=False)
+    nome = Column(String, nullable=False)
+
+    # Categoria pertence a um usuário
+    usuario_email = Column(
+        String,
+        ForeignKey("usuarios.email", ondelete="CASCADE"),
+        nullable=False,
+        index=True
+    )
