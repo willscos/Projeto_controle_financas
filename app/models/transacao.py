4f1e342f950971421c712f236a3b8ca5269dbef6
willscos <costaws2303@gmail.com>
2026-05-18 21:41:04 -0300

    Update transacao.py

--- app/models/transacao.py
+++ app/models/transacao.py
@@ -1,19 +1,19 @@
-from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from app.models.database import Base
+
 
 class Transacao(Base):
     __tablename__ = "transacoes"
 
     id = Column(Integer, primary_key=True, index=True)
-    tipo = Column(String, nullable=False)  # receita ou despesa
+    tipo = Column(String(20), nullable=False)  # receita ou despesa
     valor = Column(Float, nullable=False)
-    categoria = Column(String, nullable=False)
-    descricao = Column(String)
-    data = Column(Date, nullable=False)
-
-    # Transação pertence a um usuário
+    categoria = Column(String(100), nullable=False)
+    descricao = Column(String(255))
+    data = Column(String(20), nullable=False)  # Changed from Date to String
+    
     usuario_email = Column(
-        String,
+        String(150),
         ForeignKey("usuarios.email", ondelete="CASCADE"),
         nullable=False,
         index=True
